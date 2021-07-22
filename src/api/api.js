78dce61350d0e317f1b6bad30bac780cb42c12ca
Sergii Bogush <sergbogush@gmail.com>
2021-07-22 15:10:10 +0300

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -4,11 +4,16 @@ const API_KEY = "4423902-dbd0f970c0cc60dbb84d66d4b";
 axios.defaults.baseURL = "https://pixabay.com/api/";
 
 const getImagesByQuery = (searchQuery, currentPage) => {
-  return axios
-    .get(
-      `?image_type=photo&orientation=horizontal&q=${searchQuery}&page=${currentPage}&per_page=12&key=${API_KEY}`
-    )
-    .then((response) => response.data.hits);
+  const options = {
+    method: "get",
+    url: `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${searchQuery}&page=${currentPage}&per_page=12&key=${API_KEY}`,
+    API_KEY: "4423902-dbd0f970c0cc60dbb84d66d4b",
+  };
+
+  return axios(options).then(({ data }) => data.hits);
 };
+// .get(
+//   `?image_type=photo&orientation=horizontal&q=${searchQuery}&page=${currentPage}&per_page=12&key=${API_KEY}`
+// )
 
 export { getImagesByQuery };
